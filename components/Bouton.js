a2c4e94614efee90be03211fdbd6e911b399de50
Thomas <thomashaudiquer@gmail.com>
2022-09-28 10:25:55 +0100

    styling button

--- components/Bouton.js
+++ components/Bouton.js
@@ -5,7 +5,7 @@ function Bouton() {
   return (
     <div>
       <a href="https://doctolib.com/" target="_blank" rel="noopener noreferrer">
-      <div className="w-96 flex flex-row outline outline-2 rounded-lg hover:shadow-xl cursor-pointer place-items-center justify-center">
+      <div className="w-96 flex flex-row outline outline-2 rounded-lg hover:shadow-xl hover:bg-[#1C3879] hover:text-[#F9F5EB] cursor-pointer place-items-center justify-center">
             <p className="pr-4">Prendre un rendez-vous</p>
             <Image src={doctolib} alt='' width='80' height='80' />
       </div>
