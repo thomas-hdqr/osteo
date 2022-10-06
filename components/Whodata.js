33377981f71dac70c7a7e416ca5950c33bf138d6
Thomas <thomashaudiquer@gmail.com>
2022-10-06 15:26:18 +0100

    adding tailwind hidebar

--- components/Whodata.js
+++ components/Whodata.js
@@ -4,5 +4,20 @@ export default [
         img: "/public/me.png",
         title: "Nourrisson",
     },
+    {
+        id: 1,
+        img: "/public/me.png",
+        title: "Nourrisson",
+    },
+    {
+        id: 1,
+        img: "/public/me.png",
+        title: "Nourrisson",
+    },
+    {
+        id: 1,
+        img: "/public/me.png",
+        title: "Nourrisson",
+    },
 ]
 
