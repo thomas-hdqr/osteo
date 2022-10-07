040b31e32925a293d3d6aa730653b229cd02798b
Thomas <thomashaudiquer@gmail.com>
2022-10-07 11:05:05 +0100

    contact part almost done

--- README.md
+++ README.md
@@ -3,12 +3,14 @@ Built with Nextjs, Reactjs, Tailwindcss
 ###### Things to improve ######
 - improve responsive
     - use md: for the responsive, especially for grid layout
-- integrate Google Maps
+- fix button layout
 - add Google Analytics
 - learn gsap
 - add more framer motion
-- add a blog section with Sanity.io
 - improve seo
+- add real links
+- verify the mailto and phone link
+- add a blog section with Sanity.io
 
 
 
--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Bouton from './Bouton'
 
 
@@ -14,19 +13,20 @@ function Contact() {
       {/* Contact details and map integration */}
       <div className="grid grid-cols-2">
 
-        <div className="border-8">
+        <div>
           <div>
             <p>32 boulevard de Belgique</p>
             <p className="md:pb-10">Roanne, 42300</p>
             <p>Tél: 07 88 61 04 51</p>
             <p className="md:pb-10">Email: anaishaudiquer@gmail.com</p>
           </div>
-          <Bouton />
+            <Bouton />
         </div>
         
         {/* intégration carte Google Maps */}
-        <div className="border-8">
-
+        <div>
+          <iframe style={{width: "100%", height: "500px",}} src="https://maps.google.com/maps?q=anais%20haudiquer%20roanne&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+          </iframe>
         </div>
 
       </div>
