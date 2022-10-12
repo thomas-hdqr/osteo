6cbbbf8af8ae3bfab33bbc71ca71222f72ce74ff
Thomas <thomashaudiquer@gmail.com>
2022-10-12 11:38:19 +0100

    improving font size for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Clash Display', 'sans-serif'],
+    },
   },
   plugins: [
     require('tailwind-scrollbar-hide')
