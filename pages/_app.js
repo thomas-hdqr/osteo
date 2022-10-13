74970144e254843dc2077aa25a66fa4246d25335
Thomas <thomashaudiquer@gmail.com>
2022-10-13 18:01:09 +0100

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,10 @@
 import '../styles/globals.css'
 import '../styles/clash-display.css'
-import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-
   
-  useEffect(() => {
-    let scroll;
-    import("locomotive-scroll").then((locomotiveModule) => {
-      scroll = new locomotiveModule.default({
-        el: document.querySelector("[data-scroll-container]"),
-        smooth: true,
-        smoothMobile: false,
-        resetNativeScroll: true
-      });
-    });
-    
-    // `useEffect`'s cleanup phase
-    return () => {
-      if (scroll) scroll.destroy();
-    }
-  });
-
-
-
   return (
-      <main data-scroll-container>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
   )
 }
 
