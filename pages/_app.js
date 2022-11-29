ca027e073f8f3592a145bf9eb82cb57c4e77a23a
Thomas <thomashaudiquer@gmail.com>
2022-11-29 15:15:26 +0000

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
 import '../styles/globals.css'
 import '../styles/clash-display.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-G6N865B7PN"/>
+      <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+      __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-G6N865B7PN', {
+      page_path: window.location.pathname,
+      });
+      `,
+      }}
+      />
       <Component {...pageProps} />
+    </>
   )
 }
 
