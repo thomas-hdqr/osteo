fb096bcdf73aed23c18e15b8b939df05725f0e1c
Thomas <thomashaudiquer@gmail.com>
2022-11-29 15:12:14 +0000

    env variable added

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,10 @@ import '../styles/globals.css'
 import '../styles/clash-display.css'
 
 function MyApp({ Component, pageProps }) {
-  
   return (
       <Component {...pageProps} />
   )
 }
 
 export default MyApp
+
