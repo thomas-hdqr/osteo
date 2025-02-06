accae4a6cebda0b867ff81d682c8155fc5986901
Thomas <thomashaudiquer@gmail.com>
2025-02-06 08:55:59 +0000

    c

--- components/Bouton.js
+++ components/Bouton.js
@@ -2,10 +2,24 @@ import doctolib from '../public/doctolib.svg'
 import Image from 'next/image'
 
 function Bouton() {
+  const handleClick = () => {
+    // Send GA4 event
+    window.gtag('event', 'doctolib_click', {
+      'event_category': 'engagement',
+      'event_label': 'Doctolib Appointment Button'
+    });
+  };
+
   return (
     <div className="">
       <div className="bouton-doctolib">
-        <a href="https://www.doctolib.fr/osteopathe/roanne/anais-haudiquer-roanne" className="flex flex-row place-items-center justify-center" target="_blank" rel="noopener noreferrer">
+        <a 
+          href="https://www.doctolib.fr/osteopathe/roanne/anais-haudiquer-roanne" 
+          className="flex flex-row place-items-center justify-center" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
           <p className="pr-4 text-sm sm:text-xl font-medium">Prendre un rendez-vous</p>
           <Image src={doctolib} alt='' width='80' height='80' />
         </a>
