9fa230f7329c1e0cb9759394045ae04dcd064b4f
Thomas <thomashaudiquer@gmail.com>
2025-02-06 09:27:00 +0000

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import '../styles/clash-display.css'
 import Script from 'next/script'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <GoogleTagManager gtmId="GTM-TTFHTLZL" />
       <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=G-G6N865B7PN`}/>
       <Script strategy='lazyOnload' id=''>
         {`
--- pages/_document.js
+++ pages/_document.js
@@ -3,30 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html>
-      <Head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TTFHTLZL');`
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </Head>
-      <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TTFHTLZL"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+      <Head />
+      <body className="">
         <Main />
         <NextScript />
       </body>
